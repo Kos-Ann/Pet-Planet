a4439bb96ac184816ab61e68b65d662d004f0204
laedo303 <laedo303@mail.ru>
2024-05-16 22:14:58 +0500

    response.json() not work

--- src/index.js
+++ src/index.js
@@ -12,3 +12,25 @@ const changeActiveBtn = (event) => {
 buttons.forEach((button) => {
   button.addEventListener('click', changeActiveBtn);
 });
+// ------------------------------------------------------------------------------------
+const API_URL = 'cream-thorn-chickadee.glitch.me';
+// /api/products/category
+
+const productsList = document.querySelector('.store__list');
+
+const fetchProductsByCategory = async (category) => {
+  try {
+    const response = await fetch(`${API_URL}/api/products/category/${category}`);
+    console.log('response: ', response);
+
+    if (!response.ok) {
+      throw new Error(`Something went wrong, ${response.status}`);
+    }
+
+    console.log();
+
+  } catch (error) {
+    console.log(`Ошибка запроса товаров: ${error.message}`);
+  }
+}
+fetchProductsByCategory('Корма')

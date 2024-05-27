4cc08475fadc49ffe44fefabe1ecd6c22d07dc95
laedo303 <laedo303@mail.ru>
2024-05-27 22:33:39 +0500

    modal window styles in development

--- src/js/renderCartItems.js
+++ src/js/renderCartItems.js
@@ -1,11 +1,11 @@
 import { cartItemsList } from "../index.js"
 
 export const renderCartItems = () => {
-  cartItemsList.textContent = "";
+  // cartItemsList.textContent = "";
   const cartItems = JSON.parse(localStorage.getItem("cartItems") || "[]");
 
   cartItems.forEach((item) => {
-    const listItem = document.createElement("li");
+    // const listItem = document.createElement("li");
     listItem.textContent = item;
     cartItemsList.append(listItem);
   });
